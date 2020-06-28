ac1bfd3f6e3d55a1271724f5ae48eef79dec469b
IrelDev <ireldeveloper@gmail.com>
2020-06-28 20:14:01 +0500

    Add Example template

---
+++ Example/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Example
+//
+//  Created by Kirill Pustovalov on 28.06.2020.
+//  Copyright © 2020 Kirill Pustovalov. All rights reserved.
+//
+
+import SwiftUI
+import PieChartSwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
