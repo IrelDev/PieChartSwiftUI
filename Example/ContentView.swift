023737fc1260b025676d7c3a79019627e4cd694d
IrelDev <ireldeveloper@gmail.com>
2020-06-28 22:08:43 +0500

    Add example, add readme & license files to xcodeproj

--- Example/ContentView.swift
+++ Example/ContentView.swift
@@ -10,8 +10,51 @@ import SwiftUI
 import PieChartSwiftUI
 
 struct ContentView: View {
+    @State var indexOfSlice: String = "" //S
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        let firstItem = PieChartItemModel(value: 25, color: .pink)
+        let secondItem = PieChartItemModel(value: 25, color: .white)
+        let thirdItem = PieChartItemModel(value: 25, color: .red)
+        
+        let items = PCItems(items: [firstItem, secondItem, thirdItem])
+        
+        //PieChartView(items: PCItems(items: [25, 25, 25], chartColor: .black)) is available to use
+        return Group {
+            VStack {
+                Button(action: {
+                    guard Int(self.indexOfSlice) ?? 0 < items.items.count else { return }
+                    items.items[Int(self.indexOfSlice) ?? 0].color = .purple
+                }) {
+                    Text("Change color of item at index")
+                }
+                TextField("Enter index", text: $indexOfSlice)
+                    .frame(width: 100, height: 20, alignment: .center)
+                
+                Button(action: {
+                    guard Int(self.indexOfSlice) ?? 0 < items.items.count else { return }
+                    let newPieChartItem = PieChartItemModel(value: 25, color: items.items[Int(self.indexOfSlice) ?? 0].color)
+                    items.items[Int(self.indexOfSlice) ?? 0].subItems.append(newPieChartItem)
+                }) {
+                    Text("Add subslice to index")
+                }
+            }
+            .padding()
+            Spacer()
+            
+            PieChartView(items: items, sliceSeparatorColor: .black)
+                .frame(width: 150, height: 150, alignment: .center)
+            Spacer()
+            
+            Button(action: {
+                let newPieChartItem = PieChartItemModel(value: 25, color: .white)
+                items.items.append(newPieChartItem)
+            }) {
+                Text("Add slice")
+            }
+            .padding()
+        }
     }
 }
 
