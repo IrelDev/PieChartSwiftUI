c43977eef2d165bc555561ec18f4d7caabc0030a
IrelDev <ireldeveloper@gmail.com>
2020-06-28 19:54:50 +0500

    Initial commit

---
+++ Sources/PieChartSwiftUI.h
@@ -0,0 +1,19 @@
+//
+//  PieChartSwiftUI.h
+//  PieChartSwiftUI
+//
+//  Created by Kirill Pustovalov on 28.06.2020.
+//  Copyright © 2020 Kirill Pustovalov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PieChartSwiftUI.
+FOUNDATION_EXPORT double PieChartSwiftUIVersionNumber;
+
+//! Project version string for PieChartSwiftUI.
+FOUNDATION_EXPORT const unsigned char PieChartSwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PieChartSwiftUI/PublicHeader.h>
+
+
